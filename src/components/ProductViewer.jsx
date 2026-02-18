d8f99dc5cf810844e9a1ff78a50db3a5548bd291
kjjuythg <angon.chrisjohn014@gmail.com>
2026-02-18 17:37:31 +0800

    Current status before out

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import useMacbookStore from "../store";
+import clsx from "clsx";
+
+const ProductViewer = () => {
+  const { color, scale, setColor, setScale } = useMacbookStore();
+  return (
+    <section id="product-viewer">
+      <h2>Take a closer look</h2>
+
+      <div className="controls">
+        <p className="info"></p>
+        <p>Macbook Pro 16" in {color}</p>
+        <div className="flex-center gap-5 mt-5">
+          <div className="color-control">
+            <div
+              onClick={() => setColor("#adb5bd")}
+              className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')}
+             />
+            <div
+              onClick={() => setColor("#2e2c2e")}
+              className={clsx('bg-neutral-900', color === '#2e2c2e' && 'active')} 
+            />
+
+            
+          </div>
+
+          <div className="size-control">
+            <div>
+              <p>14"</p>
+            </div>
+            <div>
+              <p>16"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="text-white text-4xl">Render Canvas</p>
+    </section>
+  );
+};
+
+export default ProductViewer;
